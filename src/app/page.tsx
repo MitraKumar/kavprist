b940608c4a8010d5ed34326dff539f70ad120b7b
MitraKumar <kaushikmitra556@gmail.com>
2026-08-29 18:47:48 +0000

    Updated the contact us section description.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -113,9 +113,9 @@ export default function Home() {
       <div className="grid grid-cols-1 @lg:grid-cols-12 @lg:gap-4 py-16 @lg:py-4 px-6 @lg:px-0">
         <div className="col-span-1 @lg:col-start-3 @lg:col-span-4 @lg:pt-20">
           <h2 className="text-4xl @sm:text-5xl @lg:text-6xl mb-4">Contact <span className="text-primary">Us</span></h2>
-          <p className="mb-2">Ready to elevate your digital standards? Connect with our team of experts.</p>
+          <p className="mb-2">Let's build something together...</p>
           <p className="p-4 mb-4 bg-background text-white flex gap-2 items-center">
-            <RiMailLine /> <span>Email us at <a href="mailto://help@domain.com" className="text-secondary">help@domain.com</a></span>
+            <RiMailLine /> <span>Email us at <a href="mailto://help@domain.com" className="text-secondary hover:text-primary">help@domain.com</a></span>
           </p>
         </div>
         <div className="col-span-1 @lg:col-start-3 @lg:col-span-4">
